5d6d369d2aafdda2cae1f6ef7c8a82240025cb83
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-11-22 17:45:10 +0900

    Created var, val sample

---
+++ Chapter1-2/VarVal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+	val a = "Value"
+	var b = "Variable"
+	
+	println(a)
+	println(b)	
+	
+	b = "Variable -> var"
+	println(b)
+//  v Value is not overwrite	
+//	a = "Value is not Variable"
+}
