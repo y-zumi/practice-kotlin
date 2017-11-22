2a90f8520d19237126f4b50fa677d0d02fd75567
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-11-22 18:00:52 +0900

    Created When.kt

---
+++ Chapter1-2/When.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+	val color = "RED"
+		
+	when (color) {
+		"RED" -> {
+			println("Color is ${color}!")
+		}
+		else -> {
+			println("Color is None...")
+		}
+	}
+}
