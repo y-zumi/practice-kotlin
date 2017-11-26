ef20ecd87d0e6f29cf776c7b274357a11f534b82
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-11-27 01:01:50 +0900

    Created multi language hello world.

---
+++ Examples/HelloWorld/MultiLangHello.kt
@@ -0,0 +1,10 @@
+
+fun main(args: Array<String>) {
+    val language = if (args.isEmpty()) "EN" else args[0]
+    println(when (language) {
+        "EN" -> "Hello!"
+        "FR" -> "Salut!"
+        "IT" -> "Ciao!"
+        else -> "Sorry, I can't greet you in $language yet"
+    })
+}
