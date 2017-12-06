d7a3fb094bb4235fd356629aaae262e7b98aa03f
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-06 17:03:49 +0900

    Added Pyramid sample

--- KotlinInAction/Chapter2-1/When.kt
+++ KotlinInAction/Chapter2-1/When.kt
@@ -6,17 +6,20 @@ enum class Color {
 }
 fun main(args: Array<String>) {
 	val colors = arrayOf(Color.RED, Color.BLUE, Color.GREEN, Color.NONE)
-		
+
 	for (color in colors) {
 		when (color) {
 			Color.RED -> {
 				println("Color is ${color}!")
+				println("This color is normal")
 			}
 			Color.BLUE -> {
 				println("Color is ${color}!?")
+				println("This color is normal")
 			}
 			Color.GREEN -> {
 				println("Color is ${color}??")
+				println("This color is normal")
 			}
 			else -> {
 				println("Color is None...")
---
+++ Practice/Pyramid.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+  for(i in 1..4){
+	   for(j in 4 downTo 1 step 1) {
+      print(j)
+	   }
+     println()
+  }
+}
+
+
+/*
+fun main(args: Array<String>){
+	for(i in 1..4){
+		for(j in 4 downTo i){
+			print(j)
+		}
+		println()
+	}
+}
+*/
