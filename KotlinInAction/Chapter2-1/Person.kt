822f42da5251c6f7c7b63cfdd0cbc0e03a901bdc
y-zumi <b1501498@planet.kanazawa-it.ac.jp>
2017-12-07 23:37:11 +0900

    Added TryCatchAndFinally.kt

--- KotlinInAction/Chapter2-1/Person.kt
+++ KotlinInAction/Chapter2-1/Person.kt
@@ -1,5 +1,5 @@
 
-open class Person constructor(firstName: String) {
+class Person constructor(firstName: String) {
 	val name = firstName
     fun printName() {
 		println(name)
---
+++ KotlinInAction/Chapter2-1/TryCatchAndFinally.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun readNumber(reader: BufferedReader): Int? {
+    try {
+        val line = reader.readLine()
+        return Integer.parseInt(line)
+    }
+    catch (e: NumberFormatException) {
+        return null
+    }
+    finally {
+        reader.close()
+    }
+}
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(StringReader(args[0]))
+    println(readNumber(reader))
+}
